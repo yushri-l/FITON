e461527bdfe0b73270ad7cb6333e1e551f71f662
ApiramR <it23444782@my.sliit.lk>
2025-10-01 15:14:24 +0530

    Updated files

--- FITON.Server/Program.cs
+++ FITON.Server/Program.cs
@@ -6,7 +6,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.k
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
